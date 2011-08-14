e0dc0a1f737cac1ee7f8d7735921e667517b9f3e
Sebastian.Annies@gmail.com <Sebastian.Annies@gmail.com@7decde4b-c250-0410-a0da-51896bc88be6>
2011-08-14 20:37:19 +0000

    examples for track level API
    git-svn-id: http://mp4parser.googlecode.com/svn/trunk@208 7decde4b-c250-0410-a0da-51896bc88be6

---
+++ examples/src/main/java/com/googlecode/mp4parser/MuxExample.java
@@ -0,0 +1,57 @@
+package com.googlecode.mp4parser;
+
+import com.coremedia.iso.IsoBufferWrapperImpl;
+import com.coremedia.iso.IsoFile;
+import com.coremedia.iso.IsoOutputStream;
+import com.googlecode.mp4parser.authoring.Movie;
+import com.googlecode.mp4parser.authoring.Track;
+import com.googlecode.mp4parser.authoring.builder.DefaultMp4Builder;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * Muxes 2 audio tracks with a video track.
+ */
+public class MuxExample {
+    public static void main(String[] args) throws IOException {
+        IsoFile countVideoIsoFile = new IsoFile(new IsoBufferWrapperImpl(readFully(MuxExample.class.getResourceAsStream("/count-video.mp4"))));
+        countVideoIsoFile.parse();
+        Movie countVideo = new Movie(countVideoIsoFile);
+
+        IsoFile countAudioDeutschIsoFile = new IsoFile(new IsoBufferWrapperImpl(readFully(MuxExample.class.getResourceAsStream("/count-deutsch-audio.mp4"))));
+        countAudioDeutschIsoFile.parse();
+        Movie countAudioDeutsch = new Movie(countAudioDeutschIsoFile);
+
+        IsoFile countAudioEnglishIsoFile = new IsoFile(new IsoBufferWrapperImpl(readFully(MuxExample.class.getResourceAsStream("/count-english-audio.mp4"))));
+        countAudioEnglishIsoFile.parse();
+        Movie countAudioEnglish = new Movie(countAudioEnglishIsoFile);
+
+        Track audioTrackDeutsch = countAudioDeutsch.getTracks().get(0);
+        audioTrackDeutsch.getTrackMetaData().setLanguage("deu");
+        Track audioTrackEnglish = countAudioEnglish.getTracks().get(0);
+        audioTrackEnglish.getTrackMetaData().setLanguage("eng");
+
+        countVideo.addTrack(audioTrackDeutsch);
+        countVideo.addTrack(audioTrackEnglish);
+
+        IsoFile out = new DefaultMp4Builder().build(countVideo);
+        FileOutputStream fos = new FileOutputStream(new File("output.mp4"));
+        out.getBox(new IsoOutputStream(fos));
+        fos.close();
+    }
+
+
+    static byte[] readFully(InputStream is) throws IOException {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        byte[] buffer = new byte[2048];
+        int n = 0;
+        while (-1 != (n = is.read(buffer))) {
+            baos.write(buffer, 0, n);
+        }
+        return baos.toByteArray();
+    }
+}
---
+++ examples/src/main/java/com/googlecode/mp4parser/SubTitleExample.java
@@ -0,0 +1,64 @@
+package com.googlecode.mp4parser;
+
+import com.coremedia.iso.IsoBufferWrapperImpl;
+import com.coremedia.iso.IsoFile;
+import com.coremedia.iso.IsoOutputStream;
+import com.googlecode.mp4parser.authoring.Movie;
+import com.googlecode.mp4parser.authoring.TextTrackImpl;
+import com.googlecode.mp4parser.authoring.builder.DefaultMp4Builder;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * Adds subtitles.
+ */
+public class SubTitleExample {
+    public static void main(String[] args) throws IOException {
+        IsoFile countVideoIsoFile = new IsoFile(new IsoBufferWrapperImpl(readFully(SubTitleExample.class.getResourceAsStream("/count-video.mp4"))));
+        countVideoIsoFile.parse();
+        Movie countVideo = new Movie(countVideoIsoFile);
+
+        TextTrackImpl subTitleEng = new TextTrackImpl();
+        subTitleEng.getTrackMetaData().setLanguage("eng");
+        TextTrackImpl subTitleDeu = new TextTrackImpl();
+        subTitleDeu.getTrackMetaData().setLanguage("deu");
+
+        subTitleDeu.getSubs().add(new TextTrackImpl.Line(5000, 6000, "Fünf"));
+        subTitleEng.getSubs().add(new TextTrackImpl.Line(5000, 6000, "Five"));
+
+        subTitleDeu.getSubs().add(new TextTrackImpl.Line(8000, 9000, "Vier"));
+        subTitleEng.getSubs().add(new TextTrackImpl.Line(8000, 9000, "Four"));
+
+        subTitleDeu.getSubs().add(new TextTrackImpl.Line(12000, 13000, "Drei"));
+        subTitleEng.getSubs().add(new TextTrackImpl.Line(12000, 13000, "Three"));
+
+        subTitleDeu.getSubs().add(new TextTrackImpl.Line(16000, 17000, "Zwei"));
+        subTitleEng.getSubs().add(new TextTrackImpl.Line(16000, 17000, "Two"));
+
+        subTitleDeu.getSubs().add(new TextTrackImpl.Line(20000, 21000, "eins"));
+        subTitleEng.getSubs().add(new TextTrackImpl.Line(20000, 21000, "one"));
+
+        countVideo.addTrack(subTitleDeu);
+        countVideo.addTrack(subTitleEng);
+
+        IsoFile out = new DefaultMp4Builder().build(countVideo);
+        FileOutputStream fos = new FileOutputStream(new File("output.mp4"));
+        out.getBox(new IsoOutputStream(fos));
+        fos.close();
+    }
+
+
+    static byte[] readFully(InputStream is) throws IOException {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        byte[] buffer = new byte[2048];
+        int n = 0;
+        while (-1 != (n = is.read(buffer))) {
+            baos.write(buffer, 0, n);
+        }
+        return baos.toByteArray();
+    }
+}

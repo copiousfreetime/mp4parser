f4d09390c1764ceeabb1d29d13b0bb11e0d6f8a5
Sebastian.Annies@gmail.com <Sebastian.Annies@gmail.com@7decde4b-c250-0410-a0da-51896bc88be6>
2011-08-24 16:21:51 +0000

    Fragmented MP4 untested
    git-svn-id: http://mp4parser.googlecode.com/svn/trunk@216 7decde4b-c250-0410-a0da-51896bc88be6

--- examples/src/main/java/com/googlecode/mp4parser/MuxExample.java
+++ examples/src/main/java/com/googlecode/mp4parser/MuxExample.java
@@ -6,6 +6,8 @@ import com.coremedia.iso.IsoOutputStream;
 import com.googlecode.mp4parser.authoring.Movie;
 import com.googlecode.mp4parser.authoring.Track;
 import com.googlecode.mp4parser.authoring.builder.DefaultMp4Builder;
+import com.googlecode.mp4parser.authoring.builder.FragmentedMp4Builder;
+import com.googlecode.mp4parser.authoring.builder.SyncSampleIntersectFinderImpl;
 import com.googlecode.mp4parser.authoring.container.mp4.MovieCreator;
 
 import java.io.ByteArrayOutputStream;
@@ -33,10 +35,20 @@ public class MuxExample {
         countVideo.addTrack(audioTrackDeutsch);
         countVideo.addTrack(audioTrackEnglish);
 
-        IsoFile out = new DefaultMp4Builder().build(countVideo);
-        FileOutputStream fos = new FileOutputStream(new File("output.mp4"));
-        out.getBox(new IsoOutputStream(fos));
-        fos.close();
+        {
+            IsoFile out = new DefaultMp4Builder().build(countVideo);
+            FileOutputStream fos = new FileOutputStream(new File("output.mp4"));
+            out.getBox(new IsoOutputStream(fos));
+            fos.close();
+        }
+        {
+            FragmentedMp4Builder fragmentedMp4Builder = new FragmentedMp4Builder();
+            fragmentedMp4Builder.setIntersectionFinder(new SyncSampleIntersectFinderImpl());
+            IsoFile out = fragmentedMp4Builder.build(countVideo);
+            FileOutputStream fos = new FileOutputStream(new File("output-frag.mp4"));
+            out.getBox(new IsoOutputStream(fos));
+            fos.close();
+        }
     }
 
 

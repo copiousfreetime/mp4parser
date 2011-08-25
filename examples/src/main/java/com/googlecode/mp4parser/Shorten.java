b57c3b01cadbd0a488a03bddb5d42c9886700d02
sebastian.annies@gmail.com <sebastian.annies@gmail.com@7decde4b-c250-0410-a0da-51896bc88be6>
2011-08-25 08:36:50 +0000

    Fixed issue that prevented the last sample of the last chunk of each track to be written Chnaged the Shorten example to take start & end time as parameter
    git-svn-id: http://mp4parser.googlecode.com/svn/trunk@217 7decde4b-c250-0410-a0da-51896bc88be6

--- examples/src/main/java/com/googlecode/mp4parser/Shorten.java
+++ examples/src/main/java/com/googlecode/mp4parser/Shorten.java
@@ -3,33 +3,81 @@ package com.googlecode.mp4parser;
 import com.coremedia.iso.IsoBufferWrapperImpl;
 import com.coremedia.iso.IsoFile;
 import com.coremedia.iso.IsoOutputStream;
+import com.coremedia.iso.boxes.TimeToSampleBox;
 import com.googlecode.mp4parser.authoring.Movie;
 import com.googlecode.mp4parser.authoring.Track;
 import com.googlecode.mp4parser.authoring.builder.DefaultMp4Builder;
 import com.googlecode.mp4parser.authoring.container.mp4.MovieCreator;
 import com.googlecode.mp4parser.authoring.tracks.CroppedTrack;
 
-import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 
+/**
+ * Shortens/Crops a track
+ */
 public class Shorten {
     public static void main(String[] args) throws IOException {
         Movie movie = new MovieCreator().build(new IsoBufferWrapperImpl(new File("/home/sannies/suckerpunch-distantplanet_h1080p/suckerpunch-distantplanet_h1080p.mov")));
-        // I know that there is just one track
-        List<Track> nuTracks = new LinkedList<Track>();
-        for (Track track : movie.getTracks()) {
-            if (track.getType() == Track.Type.VIDEO) {
-                long startSample = track.getSyncSamples()[3];
-                nuTracks.add(new CroppedTrack(track, startSample, startSample + 250));
+
+        List<Track> tracks = movie.getTracks();
+        movie.setTracks(new LinkedList<Track>());
+        // remove all tracks we will create new tracks from the old
+
+        double startTime = 25.000;
+        double endTime = 35.000;
+
+        boolean timeCorrected = false;
+
+        // Here we try to find a track that has sync samples. Since we can only start decoding
+        // at such a sample we SHOULD make sure that the start of the new fragment is exactly
+        // such a frame
+        for (Track track : tracks) {
+            if (track.getSyncSamples() != null && track.getSyncSamples().length > 0) {
+                if (timeCorrected) {
+                    // This exception here could be a false positive in case we have multiple tracks
+                    // with sync samples at exactly the same positions. E.g. a single movie containing
+                    // multiple qualities of the same video (Microsoft Smooth Streaming file)
+
+                    throw new RuntimeException("The startTime has already been corrected by another track with SyncSample. Not Supported.");
+                }
+                startTime = correctTimeToNextSyncSample(track, startTime);
+                timeCorrected = true;
             }
         }
 
-        movie.setTracks(nuTracks);
+        for (Track track : tracks) {
+            long currentSample = 0;
+            double currentTime = 0;
+            long startSample = -1;
+            long endSample = -1;
+
+            for (int i = 0; i < track.getDecodingTimeEntries().size(); i++) {
+                TimeToSampleBox.Entry entry = track.getDecodingTimeEntries().get(i);
+                for (int j = 0; j < entry.getCount(); j++) {
+                    // entry.getDelta() is the amount of time the current sample covers.
+
+                    if (currentTime <= startTime) {
+                        // current sample is still before the new starttime
+                        startSample = currentSample;
+                    } else if (currentTime <= endTime) {
+                        // current sample is after the new start time and still before the new endtime
+                        endSample = currentSample;
+                    } else {
+                        // current sample is after the end of the cropped video
+                        break;
+                    }
+                    currentTime += (double) entry.getDelta() / (double) track.getTrackMetaData().getTimescale();
+                    currentSample++;
+                }
+            }
+            movie.addTrack(new CroppedTrack(track, startSample, endSample));
+        }
+
         IsoFile out = new DefaultMp4Builder().build(movie);
         FileOutputStream fos = new FileOutputStream(new File("output.mp4"));
         out.getBox(new IsoOutputStream(fos));
@@ -38,15 +86,27 @@ public class Shorten {
 
     }
 
-
-    static byte[] readFully(InputStream is) throws IOException {
-        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        byte[] buffer = new byte[2048];
-        int n = 0;
-        while (-1 != (n = is.read(buffer))) {
-            baos.write(buffer, 0, n);
+    private static double correctTimeToNextSyncSample(Track track, double cutHere) {
+        double[] timeOfSyncSamples = new double[track.getSyncSamples().length];
+        long currentSample = 0;
+        double currentTime = 0;
+        for (int i = 0; i < track.getDecodingTimeEntries().size(); i++) {
+            TimeToSampleBox.Entry entry = track.getDecodingTimeEntries().get(i);
+            for (int j = 0; j < entry.getCount(); j++) {
+                if (Arrays.binarySearch(track.getSyncSamples(), currentSample + 1) >= 0) {
+                    // samples always start with 1 but we start with zero therefore +1
+                    timeOfSyncSamples[Arrays.binarySearch(track.getSyncSamples(), currentSample + 1)] = currentTime;
+                }
+                currentTime += (double) entry.getDelta() / (double) track.getTrackMetaData().getTimescale();
+                currentSample++;
+            }
         }
-        return baos.toByteArray();
+        for (double timeOfSyncSample : timeOfSyncSamples) {
+            if (timeOfSyncSample > cutHere) {
+                return timeOfSyncSample;
+            }
+        }
+        return timeOfSyncSamples[timeOfSyncSamples.length - 1];
     }
 
 

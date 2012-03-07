3edd1c665182d2969e77dc308315dcd1bd92f397
Sebastian.Annies@gmail.com <Sebastian.Annies@gmail.com@7decde4b-c250-0410-a0da-51896bc88be6>
2012-03-07 17:09:24 +0000

    EC3Specific Boxen with writing
    git-svn-id: http://mp4parser.googlecode.com/svn/trunk@390 7decde4b-c250-0410-a0da-51896bc88be6

--- examples/src/main/java/com/googlecode/mp4parser/ReadWriteExample.java
+++ examples/src/main/java/com/googlecode/mp4parser/ReadWriteExample.java
@@ -6,6 +6,7 @@ import com.googlecode.mp4parser.authoring.builder.DefaultMp4Builder;
 import com.googlecode.mp4parser.authoring.builder.FragmentedMp4Builder;
 import com.googlecode.mp4parser.authoring.container.mp4.MovieCreator;
 
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.nio.channels.Channels;
@@ -20,24 +21,24 @@ public class ReadWriteExample {
     public static void main(String[] args) throws IOException {
         MovieCreator mc = new MovieCreator();
 
-        Movie video = mc.build(Channels.newChannel(ReadWriteExample.class.getResourceAsStream("/smoothstreaming/video-128h-75kbps.mp4")));
-        //Movie video = mc.build(new FileInputStream("/media/scratch/qualitaetstest_cinovu_sherminfiles/abendlandinchristenhand_1039kps.mp4").getChannel());
+        //Movie video = mc.build(Channels.newChannel(ReadWriteExample.class.getResourceAsStream("/smoothstreaming/video-128h-75kbps.mp4")));
+        Movie video = mc.build(new FileInputStream("/home/sannies/scm/svn/mp4parser/Solekai022_854_29_640x75_MaxSdSubtitle.uvu").getChannel());
 
-        IsoFile out1 = new FragmentedMp4Builder().build(video);
+        //IsoFile out1 = new FragmentedMp4Builder().build(video);
         IsoFile out2 = new DefaultMp4Builder().build(video);
 
 
-        long starttime1 = System.currentTimeMillis();
+       /* long starttime1 = System.currentTimeMillis();
         FileChannel fc1 = new RandomAccessFile("video-128h-75kbps.fmp4", "rw").getChannel();
         fc1.position(0);
         out1.getBox(fc1);
         long size1 = fc1.size();
         fc1.truncate(fc1.position());
         fc1.close();
-        System.err.println("Writing " + size1 / 1024 / 1024 + "MB took " + (System.currentTimeMillis() - starttime1));
+        System.err.println("Writing " + size1 / 1024 / 1024 + "MB took " + (System.currentTimeMillis() - starttime1));*/
 
         long starttime2 = System.currentTimeMillis();
-        FileChannel fc2 = new RandomAccessFile("video-128h-75kbps.mp4", "rw").getChannel();
+        FileChannel fc2 = new RandomAccessFile("output_uvu.mp4", "rw").getChannel();
         fc2.position(0);
         out2.getBox(fc2);
         long size2 = fc2.size();

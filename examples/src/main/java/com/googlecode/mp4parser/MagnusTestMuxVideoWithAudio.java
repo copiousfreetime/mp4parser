26e77c64f00038d1e88a63c883d005a30abdf042
hoemmagnus@gmail.com <hoemmagnus@gmail.com@7decde4b-c250-0410-a0da-51896bc88be6>
2012-03-13 14:17:30 +0000

    Added muxing of audio tracks into mp4 files.
    git-svn-id: http://mp4parser.googlecode.com/svn/trunk@429 7decde4b-c250-0410-a0da-51896bc88be6

---
+++ examples/src/main/java/com/googlecode/mp4parser/MagnusTestMuxVideoWithAudio.java
@@ -0,0 +1,48 @@
+package com.googlecode.mp4parser;
+
+import com.coremedia.iso.IsoFile;
+import com.googlecode.mp4parser.authoring.Movie;
+import com.googlecode.mp4parser.authoring.Track;
+import com.googlecode.mp4parser.authoring.builder.DefaultMp4Builder;
+import com.googlecode.mp4parser.authoring.container.mp4.MovieCreator;
+import com.googlecode.mp4parser.authoring.tracks.AC3TrackImpl;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.Channels;
+import java.util.List;
+
+
+public class MagnusTestMuxVideoWithAudio {
+
+    public static void main(String[] args) throws IOException {
+        Movie movie = new Movie();
+        Movie invideo = new MovieCreator().build(Channels.newChannel(new FileInputStream(args[0])));
+        List<Track> tracks = invideo.getTracks();
+
+        for (Track t : tracks) {
+            String type = t.getMediaHeaderBox().getType();
+            String ctype = t.getSampleDescriptionBox().getSampleEntry().getType();
+            System.out.println("Track of type " + type + " (" + ctype + ")");
+            if (type.equals("vmhd")) {
+                movie.addTrack(t);
+                System.out.println("Adding track to outputfile");
+            }
+        }
+
+        FileInputStream fin = new FileInputStream(args[1]);
+        AC3TrackImpl ac3 = new AC3TrackImpl(fin);
+        movie.addTrack(ac3);
+
+        IsoFile out = new DefaultMp4Builder().build(movie);
+        FileOutputStream fos = new FileOutputStream(new File(args[2]));
+        out.getBox(fos.getChannel());
+        fos.close();
+
+
+
+    }
+}

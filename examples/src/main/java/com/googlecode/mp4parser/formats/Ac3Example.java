68b10ca7e84d254c74d3a51e2588c3a23289951f
Sebastian.Annies@gmail.com <Sebastian.Annies@gmail.com@7decde4b-c250-0410-a0da-51896bc88be6>
2012-03-30 10:04:46 +0000

    Example/Test  for AC3 muxing
    git-svn-id: http://mp4parser.googlecode.com/svn/trunk@449 7decde4b-c250-0410-a0da-51896bc88be6

---
+++ examples/src/main/java/com/googlecode/mp4parser/formats/Ac3Example.java
@@ -0,0 +1,25 @@
+package com.googlecode.mp4parser.formats;
+
+import com.coremedia.iso.IsoFile;
+import com.googlecode.mp4parser.authoring.Movie;
+import com.googlecode.mp4parser.authoring.builder.DefaultMp4Builder;
+import com.googlecode.mp4parser.authoring.tracks.AC3TrackImpl;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+/**
+ *
+ */
+public class Ac3Example {
+    public static void main(String[] args) throws IOException {
+        AC3TrackImpl ac3Track = new AC3TrackImpl(Ac3Example.class.getResourceAsStream("/count-english.ac3"));
+        Movie m = new Movie();
+        m.addTrack(ac3Track);
+        DefaultMp4Builder mp4Builder = new DefaultMp4Builder();
+        IsoFile isoFile = mp4Builder.build(m);
+        FileOutputStream fos = new FileOutputStream("output.mp4");
+        isoFile.getBox(fos.getChannel());
+        fos.close();
+    }
+}

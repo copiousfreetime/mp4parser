33caab8bf6c7eea5127ddaa1a88a7f1a240c7cb5
Sebastian.Annies@gmail.com <Sebastian.Annies@gmail.com@7decde4b-c250-0410-a0da-51896bc88be6>
2012-05-22 09:24:38 +0000

    improve example
    git-svn-id: http://mp4parser.googlecode.com/svn/trunk@631 7decde4b-c250-0410-a0da-51896bc88be6

--- examples/src/main/java/com/googlecode/mp4parser/VideoAudioChecker.java
+++ examples/src/main/java/com/googlecode/mp4parser/VideoAudioChecker.java
@@ -12,15 +12,22 @@ public class VideoAudioChecker {
 
     public static void main(String[] args) throws IOException {
         IsoFile isoFile = new IsoFile(Channels.newChannel(SubTitleExample.class.getResourceAsStream("/count-video.mp4")));
+        System.err.println(getType(isoFile));
     }
 
-    public TYPE getType(IsoFile isoFile) {
+    public static TYPE getType(IsoFile isoFile) {
 
         List<HandlerBox> handlerBoxes =
                 isoFile.getBoxes(HandlerBox.class, true);
         for (HandlerBox handlerBox : handlerBoxes) {
-            if ("vide".equals(handlerBox.getHandlerType()))
+            if ("vide".equals(handlerBox.getHandlerType())) {
                 return TYPE.VIDEO;
+            } else if ("soun".equals(handlerBox.getHandlerType())) {
+                return TYPE.AUDIO;
+            } else {
+                System.err.println("unknown");
+            }
+
         }
         return TYPE.AUDIO;
     }

b1d07f8031dae2758238b210a7ca0de70723ce26
Sebastian.Annies@gmail.com <Sebastian.Annies@gmail.com@7decde4b-c250-0410-a0da-51896bc88be6>
2013-03-05 15:52:37 +0000

    improved H264Example, fixed H264TrackImpl added commons-io to examples
    git-svn-id: http://mp4parser.googlecode.com/svn/trunk@836 7decde4b-c250-0410-a0da-51896bc88be6

--- examples/src/main/java/com/googlecode/mp4parser/muxformats/H264Example.java
+++ examples/src/main/java/com/googlecode/mp4parser/muxformats/H264Example.java
@@ -3,15 +3,10 @@ package com.googlecode.mp4parser.muxformats;
 import com.coremedia.iso.IsoFile;
 import com.googlecode.mp4parser.authoring.Movie;
 import com.googlecode.mp4parser.authoring.builder.DefaultMp4Builder;
-import com.googlecode.mp4parser.authoring.builder.FragmentedMp4Builder;
-import com.googlecode.mp4parser.authoring.builder.SyncSampleIntersectFinderImpl;
 import com.googlecode.mp4parser.authoring.tracks.AACTrackImpl;
 import com.googlecode.mp4parser.authoring.tracks.H264TrackImpl;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
 
 /**
  * Created with IntelliJ IDEA.
@@ -22,11 +17,8 @@ import java.io.IOException;
  */
 public class H264Example {
     public static void main(String[] args) throws IOException {
-//        AACTrackImpl aacTrack = new AACTrackImpl(Ac3Example.class.getResourceAsStream("/sample.aac"));
-        H264TrackImpl h264Track = new H264TrackImpl(new FileInputStream("/Users/magnus/Projects/castlabs/cff/DTS_Paint_HD1/DTS_Paint_HD1.h264"));
-        AACTrackImpl aacTrack = new AACTrackImpl(new FileInputStream("/Users/magnus/Projects/castlabs/cff/DTS_Paint_HD1/DTS_Paint.aac").getChannel());
-//        H264TrackImpl h264Track = new H264TrackImpl(new FileInputStream("/Users/magnus/Projects/castlabs/cff/Dolby_Countdown_1920x1080p_H264_Stereo_AAC_51_DDPlus/Dolby_Countdown_1920x1080p.h264"));
-//        AACTrackImpl aacTrack = new AACTrackImpl(new FileInputStream("/Users/magnus/Projects/castlabs/cff/Dolby_Countdown_1920x1080p_H264_Stereo_AAC_51_DDPlus/Dolby_Countdown_Stereo.aac"));
+        H264TrackImpl h264Track = new H264TrackImpl(new BufferedInputStream(new FileInputStream("/home/sannies2/Downloads/lv.h264")));
+        AACTrackImpl aacTrack = new AACTrackImpl(new FileInputStream("/home/sannies2/Downloads/lv.aac").getChannel());
         Movie m = new Movie();
         m.addTrack(h264Track);
         m.addTrack(aacTrack);
@@ -37,14 +29,5 @@ public class H264Example {
             out.getBox(fos.getChannel());
             fos.close();
         }
-        {
-            FragmentedMp4Builder fragmentedMp4Builder = new FragmentedMp4Builder();
-            fragmentedMp4Builder.setIntersectionFinder(new SyncSampleIntersectFinderImpl());
-            IsoFile out = fragmentedMp4Builder.build(m);
-            FileOutputStream fos = new FileOutputStream(new File("h264_frag_output.mp4"));
-            out.getBox(fos.getChannel());
-            fos.close();
-        }
-
     }
 }

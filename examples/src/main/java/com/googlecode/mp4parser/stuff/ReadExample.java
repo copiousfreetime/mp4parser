40ce2527f8e87db10291a0b101cc26c4c89cae1c
Sebastian.Annies@gmail.com <Sebastian.Annies@gmail.com@7decde4b-c250-0410-a0da-51896bc88be6>
2013-03-18 09:41:51 +0000

    i want to release but still need that here so i check it in
    git-svn-id: http://mp4parser.googlecode.com/svn/trunk@852 7decde4b-c250-0410-a0da-51896bc88be6

--- examples/src/main/java/com/googlecode/mp4parser/stuff/ReadExample.java
+++ examples/src/main/java/com/googlecode/mp4parser/stuff/ReadExample.java
@@ -1,29 +1,24 @@
 package com.googlecode.mp4parser.stuff;
 
 import com.coremedia.iso.IsoFile;
-import com.coremedia.iso.boxes.HandlerBox;
-import com.coremedia.iso.boxes.TimeToSampleBox;
-import com.googlecode.mp4parser.authoring.Movie;
-import com.googlecode.mp4parser.authoring.container.mp4.MovieCreator;
+import com.coremedia.iso.boxes.XmlBox;
 import com.googlecode.mp4parser.util.Path;
 
+import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.RandomAccessFile;
-import java.nio.ByteBuffer;
-import java.nio.channels.FileChannel;
-import java.util.Iterator;
 
 /**
  *
  */
 public class ReadExample {
     public static void main(String[] args) throws IOException {
-        FileChannel fc = new RandomAccessFile("/media/scratch/ThreeHundredFourtyThreeMB.mp4", "rw").getChannel();
-        IsoFile isoFile = new IsoFile(fc);
-
-        HandlerBox hdlr = (HandlerBox) Path.getPath(isoFile, "/moov[0]/trak[0]/mdia[0]/hdlr[0]");
-        hdlr.setName("onetwothreefourfivesix");
-
+        FileInputStream fileInputStream = new FileInputStream("/home/sannies2/Mission_Impossible_Ghost_Protocol_Feature_SDUV_480p_16avg192max.uvu");
+        IsoFile isoFile = new IsoFile(fileInputStream.getChannel());
+        //isoFile = new IsoFile(Channels.newChannel(new FileInputStream(this.filePath)));
+        //Path path = new Path(isoFile);
+        XmlBox xmlBox = (XmlBox) Path.getPath(isoFile, "/moov/meta/xml ");
+        String xml = xmlBox.getXml();
+        System.err.println(xml);
 
 
     }
